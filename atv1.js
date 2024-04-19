e086cb614c4bbee2d3a6746fc25cf6bb45931553
Thiago Grassi <122293974+ThiagoGrassi@users.noreply.github.com>
2024-04-18 22:49:57 -0300

    Create atv1.js
    o valor será 1.

---
+++ atv1.js
@@ -0,0 +1,16 @@
+atividade1 ()
+
+function atividade1 (){
+    let  Indice = 13;         
+    let  Soma = 0; 
+    let  K = 0;
+
+
+    if (K < Indice){
+        K = K + 1;
+
+        Soma = Soma + K;
+    }
+
+    console.log(Soma)
+}
